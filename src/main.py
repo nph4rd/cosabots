ca54b9d85d942ece9f4d6c317905cb816d434949
Arturo Marquez <arturomf94@gmail.com>
2021-02-22 11:56:27 -0600

    Return sent photo

--- src/main.py
+++ src/main.py
@@ -1,28 +1,41 @@
+"""
+Handler code for the lambda
+"""
+
 import json
+
 import requests
+
 from keys import TELE_TOKEN
 
+URL = f"https://api.telegram.org/bot{TELE_TOKEN}/"
+FILE_URL = "https://api.telegram.org/file/bot{TELE_TOKEN}/"
 
-URL = "https://api.telegram.org/bot{}/".format(TELE_TOKEN)
 
 def send_message(text, chat_id):
+    """
+    Send a message with {text} to chat with {chat_id}
+    """
     final_text = "You said: " + text
-    url = URL + "sendMessage?text={}&chat_id={}".format(final_text, chat_id)
+    url = URL + f"sendMessage?text={final_text}&chat_id={chat_id}"
     requests.get(url)
 
+
 def lambda_handler(event, context):
-    message = json.loads(event['body'])
-    chat_id = message['message']['chat']['id']
-    print(chat_id)
-    # reply = message['message']['text']
-    print(message['message'])
-    image = requests.get("https://avatars.githubusercontent.com/u/9259160?s=460&u=a363b29339e611e13e1f71ea7552e8bf5914cf37&v=4")
-    file = open("/tmp/sample_image.png", "wb")
+    """
+    Handle basic bot functionality
+    """
+    message = json.loads(event["body"])
+    chat_id = message["message"]["chat"]["id"]
+    try:
+        file_id = message["photo"][0]["file_id"]
+    except KeyError as err:
+        print(f"There was no photo. Error: {err}")
+    print(message["message"])
+    image = requests.get(FILE_URL + str(file_id))
+    file = open("/tmp/image.png", "wb")
     file.write(image.content)
     file.close()
-    files = {'photo': open("/tmp/sample_image.png", "rb")}
+    files = {"photo": open("/tmp/image.png", "rb")}
     requests.post(URL + "sendPhoto?chat_id={}".format(chat_id), files=files)
-    # send_message(reply, chat_id)
-    return {
-        'statusCode': 200
-    }
+    return {"statusCode": 200}
